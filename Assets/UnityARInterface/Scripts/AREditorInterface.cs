3236f5fc755e17ffa98991897d7523c13c6760f1
StevenHodgsonCIP <steven.hodgson@codeinprogress.co.uk>
2018-04-11 08:43:29 +0100

    Use available webcam when using the Editor

--- Assets/UnityARInterface/Scripts/AREditorInterface.cs
+++ Assets/UnityARInterface/Scripts/AREditorInterface.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace UnityARInterface
 {
@@ -24,6 +25,20 @@ namespace UnityARInterface
         Vector3 m_LastMousePosition;
         Vector3 m_EulerAngles;
         Vector3[] m_PointCloud;
+
+        Matrix4x4 m_DisplayTransform;
+        WebCamTexture activeTexture;
+        ARBackgroundRenderer m_BackgroundRenderer;
+
+        void OnDestroy()
+        {
+            if (activeTexture != null)
+            {
+                activeTexture.Stop();
+                GameObject.Destroy(activeTexture);
+                activeTexture = null;
+            }
+        }
 
         public override IEnumerator StartService(Settings settings)
         {
@@ -65,7 +80,12 @@ namespace UnityARInterface
 
         public override void StopService()
         {
+            m_BackgroundRenderer.backgroundMaterial = null;
+            m_BackgroundRenderer.camera = null;
+            m_BackgroundRenderer = null;
             IsRunning = false;
+
+            UpdateCameraFeed();
         }
 
         public override bool TryGetUnscaledPose(ref Pose pose)
@@ -81,13 +101,19 @@ namespace UnityARInterface
 
         public override void SetupCamera(Camera camera)
         {
-
+            if (m_BackgroundRenderer == null)
+            {
+                m_BackgroundRenderer = new ARBackgroundRenderer();
+                m_BackgroundRenderer.backgroundMaterial = Resources.Load("Materials/ARBackground", typeof(Material)) as Material;
+                m_BackgroundRenderer.camera = camera;
+                m_BackgroundRenderer.mode = ARRenderMode.StandardBackground;
+            }
         }
 
         public override bool TryGetPointCloud(ref PointCloud pointCloud)
         {
             if (!IsRunning)
-			    return false;
+                return false;
 
             if (pointCloud.points == null)
                 pointCloud.points = new List<Vector3>();
@@ -105,10 +131,17 @@ namespace UnityARInterface
             };
         }
 
-		public override Matrix4x4 GetDisplayTransform()
-		{
-			return Matrix4x4.identity;
-		}
+        public override Matrix4x4 GetDisplayTransform()
+        {
+            if (activeTexture != null)
+            {
+                return m_DisplayTransform;
+            }
+            else
+            {
+                return Matrix4x4.identity;
+            }
+        }
 
         public override void UpdateCamera(Camera camera)
         {
@@ -152,6 +185,28 @@ namespace UnityARInterface
             {
                 m_WasMouseDownLastFrame = false;
             }
+
+            if (m_BackgroundRenderer != null)
+            {
+                var coords = new Vector2[]
+                {
+                    new Vector2(0, 1),
+                    new Vector2(1, 1),
+                    new Vector2(0, 0),
+                    new Vector2(1, 0)
+                };
+
+                var transformMatrix = GetDisplayTransform();
+
+                for (int i = 0; i < coords.Length; ++i)
+                {
+                    var transformedCoord = transformMatrix * (coords[i] - new Vector2(0.5f, 0.5f));
+                    coords[i] = new Vector2(transformedCoord.x + 0.5f, transformedCoord.y + 0.5f);
+                }
+
+                m_BackgroundRenderer.backgroundMaterial.SetVector("_UvTopLeftRight", new Vector4(coords[0].x, coords[0].y, coords[1].x, coords[1].y));
+                m_BackgroundRenderer.backgroundMaterial.SetVector("_UvBottomLeftRight", new Vector4(coords[2].x, coords[2].y, coords[3].x, coords[3].y));
+            }
         }
 
         public override void Update()
@@ -182,6 +237,82 @@ namespace UnityARInterface
                         m_State = State.Finished;
                     }
                     break;
+            }
+
+            UpdateCameraFeed();
+        }
+
+        void UpdateCameraFeed()
+        {
+            var originalActiveTexture = activeTexture;
+            var devices = WebCamTexture.devices;
+
+            if (activeTexture != null)
+            {
+                bool deviceExists = false;
+
+                if (IsRunning)
+                {
+                    for (int i = 0; i < devices.Length; i++)
+                    {
+                        if (devices[i].name == devices[i].name)
+                        {
+                            deviceExists = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!deviceExists)
+                {
+                    activeTexture.Stop();
+
+                    GameObject.Destroy(activeTexture);
+                    activeTexture = null;
+                }
+            }
+
+            if ((activeTexture == null) && IsRunning)
+            {
+                int selectedDevice = -1;
+
+                for (int i = 0; i < devices.Length; i++)
+                {
+                    if (selectedDevice == -1)
+                    {
+                        selectedDevice = i;
+                    }
+
+                    if (!devices[i].isFrontFacing)
+                    {
+                        break;
+                    }
+                }
+
+                if (selectedDevice != -1)
+                {
+                    activeTexture = new WebCamTexture(devices[selectedDevice].name, Screen.width, Screen.height, Application.targetFrameRate);
+                    activeTexture.Play();
+                }
+            }
+
+            if (activeTexture != null)
+            {
+                m_DisplayTransform = Matrix4x4.Rotate(Quaternion.Euler(0.0f, 0.0f, 90.0f + activeTexture.videoRotationAngle)) *
+                                     Matrix4x4.Scale(new Vector3(-1.0f, activeTexture.videoVerticallyMirrored ? -1.0f : 1.0f, 1.0f));
+
+                if (m_BackgroundRenderer != null)
+                {
+                    m_BackgroundRenderer.backgroundMaterial.SetTexture("_MainTex", activeTexture);
+                    m_BackgroundRenderer.mode = ARRenderMode.MaterialAsBackground;
+                }
+            }
+            else
+            {
+                if (m_BackgroundRenderer != null)
+                {
+                    m_BackgroundRenderer.mode = ARRenderMode.StandardBackground;
+                }
             }
         }
     }
